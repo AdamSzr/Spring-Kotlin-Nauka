9cdbf80a3d87af20c03a9d697a3e334e698c8a94
Adam Szreiber <aszreiber@kanga.exchange>
2022-05-20 08:56:34 +0200

    utworzony listener w 2 wersjach

--- src/main/kotlin/exchange/kanga/domain/learn/library/event/BookAddEvent.kt
+++ src/main/kotlin/exchange/kanga/domain/learn/library/event/BookAddEvent.kt
@@ -1,7 +1,8 @@
 package exchange.kanga.domain.learn.library.event
 
 import exchange.kanga.domain.learn.library.structures.BookModel
+import org.springframework.context.ApplicationEvent
 
 
-data class BookAddEvent (val book :BookModel) {
+data class BookAddEvent (val book :BookModel) : ApplicationEvent(book) {
 }
--- src/main/kotlin/exchange/kanga/domain/learn/library/listener/BookAddListener.kt
+++ src/main/kotlin/exchange/kanga/domain/learn/library/listener/BookAddListener.kt
@@ -1,14 +1,23 @@
 package exchange.kanga.domain.learn.library.listener
 
 import exchange.kanga.domain.learn.library.event.BookAddEvent
+import org.springframework.context.ApplicationListener
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class BookAddListener {
-
     @EventListener
-    fun onNewBookAdd(book:BookAddEvent){
-        println(book)
+    fun onNewBookAdd(book: BookAddEvent) {
+        println("1 ->"+book)
+    }
+
+}
+
+@Component
+class BookAddListener2: ApplicationListener<BookAddEvent> {
+
+    override fun onApplicationEvent(event: BookAddEvent) {
+        println("2 ->"+event)
     }
 }
